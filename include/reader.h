d0711a771c4cbd19c70f65a6081535c3d9474f89
Paul Bartholomew <ptb0890@gmail.com>
2024-11-01 16:30:36 +0000

    docs: Document the reader class

--- include/reader.h
+++ include/reader.h
@@ -15,25 +15,53 @@ namespace cfg
 {
   namespace reader
   {
+    /**
+     * Enum identifying the supported mesh formats that `CFGrid` reads.
+     *
+     * @note The "supported" `UNKNOWN` format is used to identify when `CFGrid` cannot identify the
+     *       mesh format and would generally result in an error being raised.
+     */
     enum class MeshFormat {GMSH_ASCII, GMSH_BIN, UNKNOWN};
     
+    /**
+     * A custom exception class that is raised by `CFGrid` when given an unknown mesh format.
+     */
     class unknown_format : public std::runtime_error
     {
     public:
       unknown_format(const std::string &msg) : std::runtime_error{msg} {};
     };
 
+    /**
+     * The reader class.
+     */
     class reader
     {
     public:
+      /**
+       * Default constructor for the reader class.
+       *
+       * @returns An uninitialised `reader` object.
+       */
       explicit reader() = default;
-      explicit reader(const std::string &meshfile) : meshfile(meshfile)
+
+      /**
+       * The constructor for the reader class.
+       *
+       * Given a path string to a mesh file the constructor performs validation that: a) the path
+       * exists, and b) the mesh file pointed to by the path is a known format according to the
+       * `MeshFormat` enum.
+       *
+       * @param[in] meshfile The path string to the mesh file to be read.
+       * @returns            An initialised `reader` object.
+       */
+      explicit reader(const std::filesystem::path meshfile) : meshfile(meshfile)
       {
 	// Confirm existence of meshfile, allowing it to throw on failure.
 	std::error_code ec;
-	if(!std::filesystem::exists(this->meshfile, ec))
+	if(!std::filesystem::exists(meshfile, ec))
 	{
-	  throw std::filesystem::filesystem_error{"Could not find mesh file " + meshfile, ec};
+	  throw std::filesystem::filesystem_error{"Could not find mesh file " + meshfile.string(), ec};
 	}
 
 	fmt = MeshFormat::UNKNOWN;
@@ -84,13 +112,18 @@ namespace cfg
 	}
       }
 
+      /**
+       * Queries the mesh format being read.
+       *
+       * @returns The `MeshFormat` enum value representing the format of the mesh being read. 
+       */
       const MeshFormat format()
       {
 	return this->fmt;
       }
     private:
-      std::filesystem::path meshfile;
-      MeshFormat fmt;
+      std::filesystem::path meshfile; // Path to the mesh to read
+      MeshFormat fmt;                 // Format of the mesh to read
     };
   }  // namespace reader
 }  // namespace cfg
