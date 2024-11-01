36d86e23793b840de8e97af92961f6fa989347b3
Paul Bartholomew <ptb0890@gmail.com>
2024-11-01 22:44:03 +0000

    refactor: Lift gmsh_header_parser implementation into source file

--- include/reader.h
+++ include/reader.h
@@ -31,78 +31,19 @@ namespace cfg
       bool binary;
       int dsize;
 
-      gmsh_header(const std::string& version,
-                  const bool& binary,
-                  const int& dsize)
-          : version(version), binary(binary), dsize(dsize){};
+      gmsh_header(const std::string& version, const bool& binary, const int& dsize) :
+          version(version), binary(binary), dsize(dsize){};
     };
 
     class gmsh_header_parser
     {
      public:
       gmsh_header_parser(const std::string& version) : version(version){};
-
-      bool is_gmsh_file(const std::filesystem::path& meshfile) const
-      {
-        std::ifstream istream(meshfile);
-        std::string line;
-        std::getline(istream, line);
-        return (line == "$MeshFormat");
-      }
-
-      const std::string get_header(const std::filesystem::path& meshfile) const
-      {
-        /*
-         * The header contents should be on line 2: discard line 1 and return
-         * line 2.
-         */
-        std::ifstream istream(meshfile);
-        std::string line;
-        std::getline(istream, line);  // discard
-        std::getline(istream, line);
-        return line;
-      }
-
-      const gmsh_header parse_header(const std::string& line) const
-      {
-        auto string2bool = [](const std::string& s) -> bool
-        {
-          /* Only strings "0", "1" are treated as valid bools */
-          if (s == "0")
-          {
-            return false;
-          }
-          else if (s == "1")
-          {
-            return true;
-          }
-          else
-          {
-            throw std::runtime_error{"Can't convert string to bool: " + s};
-          }
-        };
-
-        /* Deconstruct the header string into components. */
-        auto string2header = [string2bool](const std::string& line) -> gmsh_header
-        {
-          std::stringstream ss(line);
-          std::string ver, binflag, dsize;
-          ss >> ver >> binflag >> dsize;
-
-          return gmsh_header{ver, string2bool(binflag), std::stoi(dsize)};
-        };
-        return string2header(line);
-      }
-      const gmsh_header parse_header(const std::filesystem::path& meshfile) const
-      {
-        return parse_header(get_header(meshfile));
-      }
-
-      const bool validate_header(const gmsh_header& header) const
-      {
-        return header.version == version;
-      }
-
+      const bool is_gmsh_file(const std::filesystem::path& meshfile) const;
+      const std::string get_header(const std::filesystem::path& meshfile) const;
+      const gmsh_header parse_header(const std::string& line) const;
+      const gmsh_header parse_header(const std::filesystem::path& meshfile) const;
+      const bool validate_header(const gmsh_header& header) const;
      private:
       std::string version;  // What version is this parser for?
     };
---
+++ src/reader.cpp
@@ -0,0 +1,74 @@
+/**
+ * reader.cpp
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <reader.h>
+
+namespace cfg
+{
+  namespace reader
+  {
+    const bool gmsh_header_parser::is_gmsh_file(const std::filesystem::path &meshfile) const
+    {
+      std::ifstream istream(meshfile);
+      std::string line;
+      std::getline(istream, line);
+      return (line == "$MeshFormat");
+    }
+
+    const std::string gmsh_header_parser::get_header(const std::filesystem::path& meshfile) const
+    {
+      /*
+       * The header contents should be on line 2: discard line 1 and return
+       * line 2.
+       */
+      std::ifstream istream(meshfile);
+      std::string line;
+      std::getline(istream, line);  // discard
+      std::getline(istream, line);
+      return line;
+    }
+
+    const gmsh_header gmsh_header_parser::parse_header(const std::string& line) const
+    {
+      auto string2bool = [](const std::string& s) -> bool
+      {
+	/* Only strings "0", "1" are treated as valid bools */
+	if (s == "0")
+	{
+	  return false;
+	}
+	else if (s == "1")
+	{
+	  return true;
+	}
+	else
+	{
+	  throw std::runtime_error{"Can't convert string to bool: " + s};
+	}
+      };
+
+      /* Deconstruct the header string into components. */
+      auto string2header = [string2bool](const std::string& line) -> gmsh_header
+      {
+	std::stringstream ss(line);
+	std::string ver, binflag, dsize;
+	ss >> ver >> binflag >> dsize;
+
+	return gmsh_header{ver, string2bool(binflag), std::stoi(dsize)};
+      };
+      return string2header(line);
+    }
+    const gmsh_header gmsh_header_parser::parse_header(const std::filesystem::path& meshfile) const
+    {
+      return parse_header(get_header(meshfile));
+    }
+
+    const bool gmsh_header_parser::validate_header(const gmsh_header& header) const
+    {
+      return header.version == version;
+    }
+  }
+}
